da9cbf2128b0492cbe1f58fba30a1cca9ab1886f
elishambadi <embadi43@gmail.com>
2022-10-16 22:28:14 +0300

    fix puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,14 @@
 # Configuring an ssh client
 file_line { 'Turn off passwd auth':
-   ensure => 'created',
-   path => '/home/ubuntu/.ssh/school',
-   line => 'PasswordAuthentication no'
+   ensure => present,
+   path => '/etc/ssh/ssh_config',
+   line => 'PasswordAuthentication no',
+   multiple => true
 }
 
 file_line { 'Declare identity file':
    ensure => 'created',
-   path => '/home/ubuntu/.ssh/school',
-   line => 'IdentityFile "/home/ubuntu/.ssh/school"'
+   path => '/etc/ssh/ssh_config',
+   line => 'IdentityFile /home/ubuntu/.ssh/school',
+   multiple => true
 }
