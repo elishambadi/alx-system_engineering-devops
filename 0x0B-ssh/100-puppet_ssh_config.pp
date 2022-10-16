2b299f5dc25801e255fa471ef305a6a1f0550e86
elishambadi <embadi43@gmail.com>
2022-10-16 22:31:25 +0300

    fix puppet-lint

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,14 @@
 # Configuring an ssh client
-file_line { 'Turn off passwd auth':
-   ensure => present,
-   path => '/etc/ssh/ssh_config',
-   line => 'PasswordAuthentication no',
-   multiple => true
+file_line {'Turn off passwd auth':
+  ensure   => present,
+  path     => '/etc/ssh/ssh_config',
+  line     => 'PasswordAuthentication no',
+  multiple => true
 }
 
-file_line { 'Declare identity file':
-   ensure => 'created',
-   path => '/etc/ssh/ssh_config',
-   line => 'IdentityFile /home/ubuntu/.ssh/school',
-   multiple => true
+file_line {'Declare identity file':
+  ensure   => 'created',
+  path     => '/etc/ssh/ssh_config',
+  line     => 'IdentityFile /home/ubuntu/.ssh/school',
+  multiple => true
 }
